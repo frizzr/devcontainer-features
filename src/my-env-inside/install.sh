82c4739c285e5a1e1625c6c622cabf99e6c20446
Frizzell-Carlton, Russ <russ.frizzell-carlton@ge.com>
2025-01-08 09:25:53 -0500

    Now running things as vscode user properly.

--- src/my-env-inside/install.sh
+++ src/my-env-inside/install.sh
@@ -1,56 +1,66 @@
 #!/bin/zsh
 
-cd $HOME
+CONTAINER_USER_GROUP=$(id -gn $_CONTAINER_USER)
+cd $_CONTAINER_USER_HOME
 CREATE_LINKS_TO_USER_HOME="/usr/local/share/link-to-home.sh"
 SYNC_TO_USER_HOME="/usr/local/share/sync-to-home.sh"
 
-tee "$HOME/.zshenv" > /dev/null \
+tee "$_CONTAINER_USER_HOME/.zshenv" > /dev/null \
 << EOF
 export XDG_CONFIG_HOME=\${XDG_CONFIG_HOME:=\${HOME}/.config}
 export ZDOTDIR=\${ZDOTDIR:=\${XDG_CONFIG_HOME}/zsh}
 EOF
+chown ${_CONTAINER_USER}:${CONTAINER_USER_GROUP} "$_CONTAINER_USER_HOME/.zshenv"
 
-sudo touch $CREATE_LINKS_TO_USER_HOME
-sudo touch $SYNC_TO_USER_HOME
-sudo chmod 755 $CREATE_LINKS_TO_USER_HOME
-sudo chmod 755 $SYNC_TO_USER_HOME
+touch $CREATE_LINKS_TO_USER_HOME
+touch $SYNC_TO_USER_HOME
+chmod 755 $CREATE_LINKS_TO_USER_HOME
+chmod 755 $SYNC_TO_USER_HOME
 
-mkdir -p ./.config
-
-sudo apt update
+apt update
 export PATH="$PATH:$HOME/bin:$HOME/.local/bin"
-sudo curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 
-sudo curl -LO --output-dir /tmp https://github.com/lsd-rs/lsd/releases/download/v1.1.5/lsd-musl_1.1.5_amd64.deb
-sudo apt install /tmp/lsd-musl_1.1.5_amd64.deb
-sudo curl -LO --output-dir /tmp https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-musl_0.24.0_amd64.deb
-sudo apt install /tmp/bat-musl_0.24.0_amd64.deb
-sudo curl -LO --output-dir /tmp https://github.com/junegunn/fzf/releases/download/v0.57.0/fzf-0.57.0-linux_amd64.tar.gz
-sudo tar -C /usr/local/bin -xzf /tmp/fzf-0.57.0-linux_amd64.tar.gz
-
-# Git will be suspicious, so reassure it.
-git config --global --add safe.directory ${containerWorkspaceFolder}
+curl -LO --output-dir /tmp https://github.com/lsd-rs/lsd/releases/download/v1.1.5/lsd-musl_1.1.5_amd64.deb
+apt install /tmp/lsd-musl_1.1.5_amd64.deb
+curl -LO --output-dir /tmp https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-musl_0.24.0_amd64.deb
+apt install /tmp/bat-musl_0.24.0_amd64.deb
+curl -LO --output-dir /tmp https://github.com/junegunn/fzf/releases/download/v0.57.0/fzf-0.57.0-linux_amd64.tar.gz
+tar -C /usr/local/bin -xzf /tmp/fzf-0.57.0-linux_amd64.tar.gz
 
 # Install neovim and run it to download needed neovim plugins at build time
-sudo curl -L -o /tmp/nvim-linux64.tar.gz https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf /tmp/nvim-linux64.tar.gz
+curl -L -o /tmp/nvim-linux64.tar.gz https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+rm -rf /opt/nvim
+tar -C /opt -xzf /tmp/nvim-linux64.tar.gz
 export PATH="$PATH:/opt/nvim-linux64/bin"
 
-# Install chezmoi as non-root
-if [ -n "$DOTFILEREPO" ]; then
+###### START SECTION RUNNING AS SUPPLIED (NON-ROOT/ROOT) USER
+sudo -u $_CONTAINER_USER /bin/zsh <<EOF
+mkdir -p \$HOME/.config
+DOTFILE="$DOTFILEREPO"
+if [ -n "\$DOTFILE" ]; then
     # Install chezmoi and get dotfiles. The default dotfiles is https://github.com/frizzr/dotfiles.git, which can
     # be used by anyone as an opinionated zsh terminal IDE with neovim.
-    sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply "$DOTFILEREPO"
+    sh -c "\$(curl -fsLS get.chezmoi.io/lb)" -- init --apply "\$DOTFILE"
 fi
 
+git config --global --add safe.directory ${containerWorkspaceFolder}
+
+if [[ -e \$HOME/.zshrc ]]; then
+mv \$HOME/.zshrc \$HOME/microsoft.zshrc
+fi
+if [[ -e \$HOME/.zprofile ]]; then
+mv \$HOME/.zprofile \$HOME/microsoft.zprofile
+fi
 # Setup both neovim distros.
+export PATH="\$PATH:/opt/nvim-linux64/bin"
 NVIM_APPNAME=anvim nvim --headless +q   # stock AstroNvim
 NVIM_APPNAME=rnvim nvim --headless +q   # The author's own nvim setup
+EOF
 
 # Setup the script for linking directories from local home directory at runtime
 if [ -n "$LINKUSERCONFIGDIRS" ]; then
-sudo tee "$CREATE_LINKS_TO_USER_HOME" > /dev/null \
+tee "$CREATE_LINKS_TO_USER_HOME" > /dev/null \
 << EOF
 #!/bin/bash
 read -r -a LINK_CONFIG_ARRAY <<<"$LINKUSERCONFIGDIRS"
@@ -68,12 +78,12 @@ if [[ -e \$HOME/.config/*vim ]]; then
 vim --headless +q
 fi
 EOF
-sudo chmod 755 $CREATE_LINKS_TO_USER_HOME
+chmod 755 $CREATE_LINKS_TO_USER_HOME
 fi
 
 # Setup the script for copying directory contents from local home directory at runtime
 if [ -n "$COPYUSERCONFIGDIRS" ]; then
-sudo tee "$SYNC_TO_USER_HOME" > /dev/null \
+tee "$SYNC_TO_USER_HOME" > /dev/null \
 << EOF
 #!/bin/bash
 read -r -a COPY_CONFIG_ARRAY <<<"$COPYUSERCONFIGDIRS"
@@ -86,6 +96,6 @@ if [[ -e \$HOME/.config/*nvim ]]; then
 nvim --headless +q
 fi
 EOF
-sudo chmod 755 $SYNC_TO_USER_HOME
+chmod 755 $SYNC_TO_USER_HOME
 fi
 
